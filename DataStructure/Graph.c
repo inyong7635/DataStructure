4ba8c6bd0592a24f38bde30bb56f1d332dea3b03
inyong7635 <42531525+inyong7635@users.noreply.github.com>
2019-01-07 20:19:44 +0900

    2019.01.07 v1

--- DataStructure/Graph.c
+++ DataStructure/Graph.c
@@ -8,3 +8,21 @@ Graph * CreatGraph()
 void DestroyGraph(Graph * graph)
 {
 }
+
+Vertex * CreateVertex(ElementType data)
+{
+	return NULL;
+}
+
+void DestroyVertex(Vertex * vertex)
+{
+}
+
+Edge * CreateEdge(Vertex * from, Vertex * to, int weight)
+{
+	return NULL;
+}
+
+void DestroyEdge(Edge * edge)
+{
+}
